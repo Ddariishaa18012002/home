cfab394a672fe87970ed32ad92a8185584f1811c
petrova <dasha.perova.02@inbox.ru>
2024-05-17 21:46:18 +0700

    Pipe в одном процессе и родственные процессы pipe

--- pipe.c
+++ pipe.c
@@ -6,31 +6,29 @@
 int main() {
         int fd[2];
         size_t size;
-        char string[] = "Hello from pipe!\n";
-        char resstring[17];
+        char string[] = "Hello, pipe!";
+        char resstring[14];
 
         if (pipe(fd)<0){
-                 printf("Ne udalos sozdat pipe\n");
+                 printf("Не создается pipe\n");
                  exit(-1);
         }
-        size = write(fd[1], string, 17);
-        if (size != 17) {
-                  printf("Ne udalos zapysat 17 byte\n");
+        size = write(fd[1], string, 14);
+        if (size != 14) {
+                  printf("Не получается создать строку\n");
                   exit(-1);
         }
-        size = read(fd[0], resstring, 17);
-        if (size != 17) {
-                  printf("Ne udalos prochitat 17 byte iz pipe\n");
+        size = read(fd[0], resstring, 14);
+        if (size < 0) {
+                  printf("Не получается прочитать строку\n");
                   exit(-1);
         }
-        printf("Prochitannaya stroka: %s\n", resstring);
+        printf("%s\n", resstring);
         if(close(fd[0])<0) {
-                  printf("Ne udalos zakryt pipe dlya chtenya\n");
-                  exit(-1);
+                  printf("Не закрывается входной поток\n");
         }
         if(close(fd[0])<0) {
-                  printf("Ne udalos zakryt pipe dlya zapisi\n");
-                  exit(-1);
+                  printf("Не закрывается выходной поток \n");
         }
 return 0;
 }
