aa9d49fac4b2e7532db71642401e8bc3238130c6
petrova <dasha.perova.02@inbox.ru>
2024-05-21 18:56:44 +0700

    Добавили программу создающую .txt документ

--- wrfile.c
+++ wrfile.c
@@ -7,36 +7,20 @@
 int main() {
         int fd;
         size_t size;
-        char string[] = "Knee-deep in the Dead";
-        if((fd = open("hello.txt", O_WRONLY|O_CREAT, 0666)) < 0) {
-                printf("too bad\n");
-                exit(-1);
-        }
-        size = write(fd, string, 21);
+	char string[] = "Knee-deep in the Dead";
+	if((fd = open("hello.txt", O_WRONLY|O_CREAT, 0666)) < 0) {
+		printf("too bad\n");
+		exit(-1);
+	}
+	size = write(fd, string, 21);
 	if(size != 21) {
 		printf("Не удалось записать 21 байт\n");
 		exit(-1);
-        }
-        else printf("Строка записана\n");
+	}
+	else printf("Строка записана\n");
 	if (close(fd) < 0) {
 		printf("Не получилось закрыть\n");
 		exit(-1);
-        }
-        char resstring[21];
-	if((fd = open("hello.txt", O_RDONLY)) < 0) {
-                printf("Ошибка записи для чтения\n");
-		exit(-1);
-        }
-        size = read(fd, resstring, 21);
-	if (size != 21) {
-		printf("epic failure\n");
-		exit(-1);
-        }
-        else printf("Всё получилось: %s\n", resstring);
-	if (close(fd) < 0) {
-		printf("Не получилось закрыть\n");
-		exit(-1);
-	return 0;
 	}
 	return 0;
 }
