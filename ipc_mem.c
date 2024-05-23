da171cc753819c0f9f90ad7c891b58bb1d375a39
petrova <dasha.perova.02@inbox.ru>
2024-05-23 23:37:37 +0700

    Добавили программы, работающие через разделяемую память

--- ipc_mem.c
+++ ipc_mem.c
@@ -11,44 +11,41 @@ int main() {
         int new = 1;
         char pathname[] = "fifo.c";
         key_t key;
-        if((key = ftok(pathname,0)) < 0) {
-                printf("Не удалось сгенерировать ip-ключ\n");
-                exit(-1);
-        }
-        if((shmid = shmget(key, 3*sizeof(int), 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
-                if(errno != EEXIST) {
-                        printf("Не удалось создать сегмент разделяемой памяти\n");
-                        exit(-1);
-                }
-                else {
-                        if((shmid = shmget(key, 3*sizeof(int), 0)) < 0) {
-                                printf("Не удается найти сегмент разделяемой памяти\n");
-                                exit(-1);
-                        }
-                        new = 0;
-                }
-        }
-        if ((array = (int *)shmat(shmid, NULL, 0 )) == (int *)(-1)) {
-                printf("Не удалось присоединить сегмент разделяемой памяти к адр.пространству процесса\n");
-                exit(-1);
-        }
-        if(new) {
-                array[0] = 1;
-                array[1] = 0;
-                array[2] = 1;
-        }
-        else {
-                array[0] += 1;
-                array[2] += 1;
-        }
-        printf("Программа 1 была запущена %d раз(а), общее количество запусков обоих программ: %d\n", array[1], array[2]);
-        if(shmdt(array) < 0) {
-                printf("Не удалось удалить сегмент разделяемой памяти из адр. пространства текущего процесса\n");
-                exit(-1);
-        }
-        return 0;
-} 
-
-
-
+        if((key = ftok(pathname, 0)) < 0) {
+		printf("Не удалось создать ip ключ\n");
+		exit(-1);
+	}
+	if((shmid = shmget(key, 3*sizeof(int), 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
+		if(errno != EEXIST) {
+			printf("Не удалось создать сегмент\n");
+			exit(-1);
+		}
+		else {
+			if((shmid = shmget(key, 3*sizeof(int), 0)) < 0) {
+				printf("Не удается найти сегмент\n");
+				exit(-1);
+			}
+			new = 0;
+		}
+	}
+	if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
+		printf("Не удалось присоединить сегмент\n");
+		exit(-1);
+	}
+	if(new) {
+		array[0] = 1;
+		array[1] = 0;
+		array[2] = 1;
+	}
+	else {
+		array[0] += 1;
+		array[2] += 1;
+	}
+	printf("Программа 1 запустилась %d раз(а)\nОбщее кол-во запусков: %d\n", array[0], array[2]);
+	if(shmdt(array) < 0) {
+		printf("Не удалось удалить сегмент\n");
+		exit(-1);
+	}
+	return 0;
+}
 
